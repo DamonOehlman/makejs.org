6207762de060a9421eee0cd4eb24f63144bd2a22
Damon Oehlman <damon.oehlman@gmail.com>
2014-02-22 01:30:34 +1100

    Adjusted the approach so I can add more content to the start of the body

--- enwonder.js
+++ enwonder.js
@@ -1,20 +1,25 @@
+var through = require('through');
 var tr = require('trumpet')();
 var fs = require('fs');
 var path = require('path');
-var styleWriter = tr.select('head style').createWriteStream();
+var body = tr.select('body').createStream();
+var css = fs.readFileSync(__dirname + '/style.css', { encoding: 'utf8' });
+var modified;
+
+function write(data) {
+  modified = modified || this.queue('\n<style>\n' + css + '\n</style>');
+  this.queue(data);
+}
+
+function end() {
+  this.queue(null);
+}
+
+// transform the body
+body.pipe(through(write, end)).pipe(body);
 
 // pipe the output of trumpet to the index.html file
 tr.pipe(fs.createWriteStream(__dirname + '/index.html'));
 
 // read the make input file and pipe into trumpet
 fs.createReadStream(__dirname + '/make.html').pipe(tr);
-
-// read the style in manually, as piping into the trumpet stream seems
-// to break things :/
-fs.readFile(__dirname + '/style.css', { encoding: 'utf8' }, function(err, css) {
-  if (err) {
-    return console.error(err);
-  }
-
-  styleWriter.end(css);
-});
